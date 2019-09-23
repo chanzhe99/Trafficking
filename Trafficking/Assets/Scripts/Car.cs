0e9163b31a50f5b6de99bd2266f63309aeba5659
Ivan Ong <pretzelweerum@hotmail.com>
2019-09-24 00:54:21 +0800

    Updated Pathing system and car movement

---
+++ Trafficking/Assets/Scripts/Car.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public Transform path;
+    public float carSpeed;
+
+    [SerializeField] private List<Transform> nodes;
+    private int currentNode = 0;
+
+    private void Start()
+    {
+        Transform[] pathTransform = path.GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+
+        for (int i=0; i<pathTransform.Length; i++)
+        {
+            if (pathTransform[i] != path.transform)
+            {
+                nodes.Add(pathTransform[i]);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        Drive();
+        CheckWaypointDistance();
+    }
+
+    private void Drive()
+    {
+        transform.position = Vector3.Lerp(transform.position, nodes[currentNode].position, Time.deltaTime * carSpeed);
+        //transform.Translate(nodes[currentNode].position * Time.deltaTime);
+    }
+
+    private void CheckWaypointDistance()
+    {
+        if (Vector3.Distance(transform.position, nodes[currentNode].position) < 0.05f)
+        {
+            //if (currentNode == nodes.Count - 1)
+            //{
+            //    currentNode = 0;
+            //}
+            //else
+            //{
+            //    currentNode++;
+            //}
+            if (currentNode < nodes.Count)
+            {
+                currentNode++;
+            }
+        }
+    }
+
+    private void Rotate()
+    {
+
+    }
+}
--- Trafficking/Assets/Scripts/Pathing.cs
+++ Trafficking/Assets/Scripts/Pathing.cs
@@ -25,16 +25,17 @@ public class Pathing : MonoBehaviour
         for (int i = 0; i < nodes.Count; i++)
         {
             Vector3 currentNode = nodes[i].position;
-            Vector3 previousNode = Vector3.zero;
+            Vector3 previousNode = nodes[0].position;
+            //Vector3 previousNode = Vector3.zero;
 
             if (i>0)
             {
                 previousNode = nodes[i - 1].position;
             }
-            else if (i == 0 && nodes.Count > 1)
-            {
-                previousNode = nodes[nodes.Count - 1].position;
-            }
+            //else if (i == 0 && nodes.Count > 1)
+            //{
+            //    previousNode = nodes[nodes.Count - 1].position;
+            //}
             Gizmos.DrawLine(previousNode, currentNode);
             Gizmos.DrawWireSphere(currentNode, 0.3f);
         }
