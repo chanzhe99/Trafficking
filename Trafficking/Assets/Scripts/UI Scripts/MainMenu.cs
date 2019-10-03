4b0c809c0b100676ecd5e459f78429be18830982
chanzhe <chanzhe99@gmail.com>
2019-10-03 15:40:30 +0800

    Main menu integrated

---
+++ Trafficking/Assets/Scripts/UI Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject SettingsPanelUI;
+
+    public void OpenSettings()
+    {
+        SettingsPanelUI.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        SettingsPanelUI.SetActive(false);
+    }
+
+    //public enum Scene
+    //{
+        
+    //}
+
+    //public static void LoadMenu(Scene scene)
+    //{
+    //    SceneManager.LoadScene(scene.ToString());
+    //}
+}
