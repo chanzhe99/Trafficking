d0cd3e88e7bd799530b1dfeec2b30386c8266bb2
Kenxaz <ribyjoey@hotmail.com>
2019-11-12 13:11:20 +0800

    Tweaked the UI, added PauseMenu UI to Level 1

---
+++ Trafficking/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject PauseMenuUI;
+
+    public void OpenPauseMenu()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+    public void OpenSettings()
+    {
+
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Back()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+}
