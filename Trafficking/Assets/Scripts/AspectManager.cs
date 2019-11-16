f2e2bada6c88980d80a92779ffad173a05d818c0
Kenxaz <ribyjoey@hotmail.com>
2019-11-16 16:10:56 +0800

    Some UI changes

---
+++ Trafficking/Assets/Scripts/AspectManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AspectManager : MonoBehaviour
+{
+    [SerializeField] GameObject MainMenuUI;
+
+    private void Update()
+    {
+        checkResolution();
+    }
+
+    private void checkResolution()
+    {
+        if (Screen.width == 1280 && Screen.height == 800)
+        {
+            MainMenuUI.GetComponent<AspectRatioFitter>().aspectRatio = 1.6f;
+        }
+        else if(Screen.width == 1024 && Screen.height == 600)
+        {
+            MainMenuUI.GetComponent<AspectRatioFitter>().aspectRatio = 1.71f;
+        }
+        else if(Screen.width == 972 && Screen.height == 486)
+        {
+            MainMenuUI.GetComponent<AspectRatioFitter>().aspectRatio = 2f;
+        }
+        else if(Screen.width == 864 && Screen.height == 486)
+        {
+            MainMenuUI.GetComponent<AspectRatioFitter>().aspectRatio = 1.78f;
+        }
+        else if(Screen.width == 2048 && Screen.height == 1536)
+        {
+            MainMenuUI.GetComponent<AspectRatioFitter>().aspectRatio = 1.34f;
+        }
+    }
+}
