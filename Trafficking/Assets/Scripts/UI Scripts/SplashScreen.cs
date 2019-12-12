6ae508ee008c649e5226a320a6e296e2c7e7629b
Ivan Ong <pretzelweerum@hotmail.com>
2019-12-12 18:16:22 +0800

    Added splash screen (KDU logo, UoW logo and team logo in)

---
+++ Trafficking/Assets/Scripts/UI Scripts/SplashScreen.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SplashScreen : MonoBehaviour
+{
+    public CanvasGroup kduLogoCanvas;
+    public CanvasGroup uowLogoCanvas;
+    public CanvasGroup teamLogoCanvas;
+
+    private void Start()
+    {
+        StartCoroutine(Transition());
+    }
+
+    IEnumerator Transition()
+    {
+        for(float f = 0f; f <= 1; f += Time.deltaTime)
+        {
+            kduLogoCanvas.alpha = f;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f);
+        
+        for (float f = 1f; f >= 0; f -= Time.deltaTime)
+        {
+            kduLogoCanvas.alpha = f;
+            yield return null;
+        }
+        yield return new WaitForSeconds(1f);
+
+
+        for(float f = 0f; f <= 1; f += Time.deltaTime)
+        {
+            uowLogoCanvas.alpha = f;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f);
+        
+        for (float f = 1f; f >= 0; f -= Time.deltaTime)
+        {
+            uowLogoCanvas.alpha = f;
+            yield return null;
+        }
+        yield return new WaitForSeconds(1f);
+        
+        for(float f = 0f; f <= 1; f += Time.deltaTime)
+        {
+            teamLogoCanvas.alpha = f;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f);
+        
+        for (float f = 1f; f >= 0; f -= Time.deltaTime)
+        {
+            teamLogoCanvas.alpha = f;
+            yield return null;
+        }
+        yield return new WaitForSeconds(1f);
+
+        LoadTitle();
+    }
+
+    IEnumerator FadeInFadeOut(CanvasGroup canvas)
+    {
+        for (float f = 0f; f <= 1; f += Time.deltaTime)
+        {
+            canvas.alpha = f;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        //fade
+        for (float f = 1f; f >= 0; f -= Time.deltaTime)
+        {
+            canvas.alpha = f;
+            yield return null;
+        }
+        yield return new WaitForSeconds(1f);
+        LoadTitle();
+    }
+
+
+    private void LoadTitle()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
